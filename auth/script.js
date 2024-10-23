6de5a6843fab62b501f90125255b9e09dde25f57
Ruslan Akhmejanov <akhme@MacBook-Air.local>
2024-10-22 20:10:55 -0400

    Added sign-in/up pages

---
+++ auth/script.js
@@ -0,0 +1,30 @@
+// const nums = [15, 6, 42, 8, 23, 17, 9, 31, 3, 10];
+
+// const squared_nums = nums.map(num => num * num);
+// console.log(squared_nums);
+
+// const even_nums = nums.filter(num => num % 2 === 0);
+// console.log(even_nums);
+
+// const sum_of_even_nums = even_nums.reduce((accum, currentNum) => accum + currentNum, 0);
+// console.log(sum_of_even_nums);
+
+// const elements = [15, 6, 42, 8, "hi", 17, 9, 31, 3, 10];
+
+// const answer = elements.every(num => num > 2);
+// console.log(answer);
+
+// class Car {
+
+//     constructor(brand, speed) {
+//         this.brand = brand;
+//         this.speed = speed;
+//     }
+
+//     getBrand() {
+//         return this.brand;
+//     }
+
+// }
+
+// const car = new Car("Toyota", 100);
