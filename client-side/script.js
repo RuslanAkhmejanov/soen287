f2fc76c50710617a15e678582ee2cf0f99b5fa20
Ruslan Akhmejanov <akhme@MacBook-Air.local>
2024-10-31 22:12:33 -0400

    Linked accound.html to home.html

--- client-side/script.js
+++ client-side/script.js
@@ -1,31 +1,31 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener('DOMContentLoaded', function() {
 
     // to update the UI based on the logged-in user
     function updateButtons() {
-        const loggedInUser = JSON.parse(localStorage.getItem("loggedInUser"));
+        const loggedInUser = JSON.parse(localStorage.getItem('loggedInUser'));
 
-        const authButtons = document.getElementById("authentication-buttons");
-        const signOutButton = document.getElementById("sign-out-button");
+        const authButtons = document.getElementById('authentication-buttons');
+        const accountButtons = document.getElementById('account-buttons');
 
         if (loggedInUser) {
-            authButtons.classList.add('hidden-auth-button');
-            signOutButton.classList.remove('hidden-auth-button');
+            authButtons.classList.add('hidden-buttons');
+            accountButtons.classList.remove('hidden-buttons');
         } else {
-            authButtons.classList.remove('hidden-auth-button');
-            signOutButton.classList.add('hidden-auth-button');
+            authButtons.classList.remove('hidden-buttons');
+            accountButtons.classList.add('hidden-buttons');
         }
     }
 
     function handleSignOut(event) {
         event.preventDefault();
-        localStorage.removeItem("loggedInUser");
+        localStorage.removeItem('loggedInUser');
         updateButtons();
         window.location.href = "../auth/signin.html"; // Redirect to sign in page
     }
 
-    const signOutLink = document.getElementById("sign-out-link");
+    const signOutLink = document.getElementById('sign-out-link');
     if (signOutLink) {
-        signOutLink.addEventListener("click", handleSignOut);
+        signOutLink.addEventListener('click', handleSignOut);
     }
 
     updateButtons();
