237522aab466b934ed6bdc8dfc1887b57f7f23ff
Ruslan Akhmejanov <akhme@MacBook-Air.ht.home>
2024-11-21 02:29:38 -0500

    Updated db migrations

---
+++ migrations/20241119033202-create_users.cjs
@@ -0,0 +1,45 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING(50),
+        allowNull: false
+      },
+      username: {
+        type: Sequelize.STRING(100),
+        unique: true,
+        allowNull: false
+      },
+      password: {
+        type: Sequelize.TEXT,
+        allowNull: false
+      },
+      isAdmin: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Users');
+  }
+};
