e7a69dc6bf7c4f1884538ef1f98bc2a5542ef9e7
Ruslan Akhmejanov <akhme@MacBook-Air.local>
2024-11-22 13:17:26 -0500

    Small mistake

--- public/client-side/script.js
+++ public/client-side/script.js
@@ -1,22 +1,25 @@
 document.addEventListener('DOMContentLoaded', function() {
     // overriding the dafault behaivour of the anchor tag that acts as a logout button
-    document.getElementById('sign-out-link').addEventListener('click', function (event) {
-        event.preventDefault();
-
-        // create a form element dynamically
-        var form = document.createElement('form');
-        form.method = 'POST';
-        form.action = '/signout';
-
-        // Optionally add CSRF token for security (optional but recommended)
-        // var csrfTokenInput = document.createElement('input');
-        // csrfTokenInput.type = 'hidden';
-        // csrfTokenInput.name = '_csrf';  // CSRF token field (depends on your CSRF library)
-        // csrfTokenInput.value = '<%= csrfToken %>'; // CSRF token value injected by the server
-        // form.appendChild(csrfTokenInput);
-
-        // append the form to the body and submit it
-        document.body.appendChild(form);
-        form.submit();
-    });
+    const signOutLink = document.getElementById('sign-out-link');
+    if (signOutLink) { // it is only there when a user is signed in
+        signOutLink.addEventListener('click', function (event) {
+            event.preventDefault();
+    
+            // create a form element dynamically
+            var form = document.createElement('form');
+            form.method = 'POST';
+            form.action = '/signout';
+    
+            // Optionally add CSRF token for security (optional but recommended)
+            // var csrfTokenInput = document.createElement('input');
+            // csrfTokenInput.type = 'hidden';
+            // csrfTokenInput.name = '_csrf';  // CSRF token field (depends on your CSRF library)
+            // csrfTokenInput.value = '<%= csrfToken %>'; // CSRF token value injected by the server
+            // form.appendChild(csrfTokenInput);
+    
+            // append the form to the body and submit it
+            document.body.appendChild(form);
+            form.submit();
+        });
+    }
 });
